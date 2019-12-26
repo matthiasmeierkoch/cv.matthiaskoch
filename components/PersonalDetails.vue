9606ad944c00757c6907868ddab7fdbbf7217483
Matthias Koch <matthiasmeierkoch@hotmail.com>
2019-12-26 20:05:56 +0100

    Made a lot of different changes including: ADD: Personal Details Fix: Layout & Typo ADD: SAL ADD: Multipage

---
+++ components/PersonalDetails.vue
@@ -0,0 +1,132 @@
+<template>
+    <div class="content">
+        <div class="Section">
+            <div>
+                <img class="portrait" src="../assets/matthiaskoch.jpg">
+            </div>
+
+            <h2 class="Personal__title">{{ title }}</h2>
+            <div class="Personal__subtitle">
+                <h3>Name:</h3>
+                <h3>Mail:</h3>
+                <h3>Residence:</h3>
+                <h3>Date of birth:</h3>
+                <h3>Nationality:</h3>
+                <h3>Hobbys:</h3>
+            </div>
+
+            <div class="Personal__content info">
+                <p>Matthias Koch</p>
+                <p><a href="mailto:hello@matthiasko.ch">hello@matthiasko.ch</a></p>
+                <p>Zug, Switzerland</p>
+                <p>23.04.1996</p>
+                <p>Swiss</p>
+                <p>⛷🧗🏽‍♂️🏋🏽🗺📷</p>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        props: ['title']
+    }
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap');
+    @import url('https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap');
+
+
+    .Section {
+        position: relative;
+        justify-content: space-between;
+        display: flex;
+    }
+
+    .Personal__title {
+        width: 100%;
+        padding-top: 0.25rem;
+        font-size: 4.5rem;
+        font-family: 'Montserrat', sans-serif;
+        font-weight: 700;
+        position: absolute;
+        color: RGBA(49, 79, 64, 0.15);
+
+    }
+
+    .Personal__subtitle {
+        font-family: 'Nunito', sans-serif;
+        font-weight: 700;
+        font-size: 1.125rem;
+        line-height: 1.575rem;
+        width: 40%;
+        margin-top: 7.5rem;
+    }
+
+    .Personal__content {
+        font-family: 'Nunito', sans-serif;
+        font-size: 1.125rem;
+        line-height: 1.575rem;
+        padding-bottom: 1.5rem;
+        font-weight: 400;
+        width: 120%;
+        margin-top: 7.5rem;
+
+    }
+
+    .info p {
+        padding-bottom: 0.76rem;
+    }
+
+    .Personal__subtitle h3{
+        padding-bottom: 0.75rem;
+    }
+
+    .content {
+        margin-top: -4rem;
+    }
+
+    .portrait{
+        position: absolute;
+        margin-top: 4.5rem;
+        right: 2rem;
+        width: 37%;
+    }
+
+    @media (max-width: 667px) {
+
+        .Section {
+            display: contents;
+        }
+
+        .Personal__title {
+            width: 100%;
+            margin-top: -37.25rem;
+            position: absolute;
+        }
+
+        .Personal__subtitle{
+            width: 100%;
+            margin-top: 3rem;
+            position: relative;
+        }
+
+        .Personal__content {
+            width: 100%;
+            margin-top: 3rem;
+            position: relative;
+
+        }
+
+        .portrait{
+            position: relative;
+            margin-top: 10.6rem;
+            width: 100%;
+            right: 0;
+        }
+    }
+
+</style>
---
+++ components/contentSection.vue
@@ -0,0 +1,141 @@
+<template>
+
+        <div class="sectionContent">
+            <h2 class="lead" data-sal="slide-up" data-sal-delay="100" data-sal-duration="1000" data-sal-easing="ease-out-bounce">Thank you for showing interest in my CV, if you have any Questions, inquiries or would like
+                to meet up for a Coffee please do not hesitate to Contact me.
+                Cheers, Matthias!</h2>
+            <hr>
+            <PersonalDetails title="Personal Details">
+            </PersonalDetails>
+
+            <hr>
+
+            <Section title="Experience">
+                <p class="what"><strong>Interaction Designer</strong><br>@ Swisscom ag (2019 - now)
+                </p>
+
+                <p class="what"><strong>Interaction Design & Photography </strong><br>@ internezzo ag (2017 - 2019)
+                </p>
+
+                <p class="what"><strong>Event Management Internship & Photography</strong>
+                    <br>@ Küssenberger & Partner (2016 - 2017)</p>
+
+                <p class="what"><strong>Fotofachmann EFZ</strong>
+                    <br>@ Foto Studio Kasino ag (2012 - 2015)</p>
+            </Section>
+
+            <hr>
+
+            <Section title="Education">
+                <p class="what"><strong>HF Interaction Design</strong>
+                    <br>@ Schule für Gestaltung Zürich (2017 - 2020)</p>
+
+                <p class="what"><strong>Cambridge First Certificate</strong>
+                    <br>@ Selc Syndey, Australia (2015 - 2016)</p>
+
+                <p class="what"><strong>Fotofachmann EFZ</strong>
+                    <br>@ Medien Form & Farbe (2012 - 2015)</p>
+            </Section>
+
+            <hr>
+
+            <Section title="Skills">
+                <p><strong>Design:</strong><br> Sketch, Figma, Adobe XD, Protopie, Adobe Photoshop, Illustrator,
+                    InDesign, Premiere Pro,
+                    After Effects</p>
+                <p><strong>Code:</strong><br> Basic HTML, CSS and Javascript</p>
+                <p><strong>Tools I like:</strong><br> Jira, Confluence, Slack, Bear</p>
+            </Section>
+
+            <hr>
+
+            <Section title="Languages">
+                <p>Swiss German: Native</p>
+                <p>German: Fluent</p>
+                <p>English: Fluent</p>
+            </Section>
+
+            <hr>
+
+            <h2 class="contact">Matthias Koch // <a href="mailto:hello@matthiasko.ch">hello@matthiasko.ch</a></h2>
+        </div>
+
+
+
+</template>
+
+
+<script>
+
+    import Section from "../components/Section";
+    import sal from 'sal.js';
+    import PersonalDetails from "../components/PersonalDetails";
+
+    export default {
+        components: {
+            Section,
+            PersonalDetails
+        },
+        mounted () {
+            this.$nextTick(() => sal());
+        }
+    }
+
+</script>
+
+
+<style lang="scss">
+    @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap');
+
+    .lead {
+        font-family: 'Nunito', sans-serif;
+        font-weight: 300;
+        font-size: 1.625rem;
+        margin: auto;
+        padding-top: 11.25rem;
+    }
+
+    .sectionContent {
+        max-width: 56.875rem;
+        margin: 0 auto;
+        background: white;
+
+    }
+
+    hr {
+        margin: auto;
+        margin-top: 6.25rem;
+        margin-bottom: 3.125rem;
+        border: 1x solid #CCCCCC;
+    }
+
+    p {
+        font-family: 'Nunito', sans-serif;
+        font-size: 1.125rem;
+        line-height: 1.575rem;
+        padding-bottom: 1.5rem;
+    }
+
+
+    h3 {
+        font-family: 'Nunito', sans-serif;
+        font-size: 1.325rem;
+    }
+
+    h2 {
+        font-family: 'Nunito', sans-serif;
+        font-size: 1.825rem;
+    }
+
+    .contact {
+        font-family: 'Nunito', sans-serif;
+        font-weight: 300;
+        font-size: 1.125rem;
+        margin-bottom: 3.125rem;
+    }
+
+    html {
+        overflow-x: hidden;
+    }
+
+</style>
